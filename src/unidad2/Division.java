7db4ab2e2d6cf67468ef758f78e7c08d3705d0ff
Julio Pedro López Bayón <jplbayon@gmail.com>
2020-10-21 17:28:43 +0200

    Ejercicio 1 de la unidad 1 resuelto

---
+++ src/unidad2/Division.java
@@ -0,0 +1,12 @@
+package unidad2;
+
+public class Division {
+
+	public static void main(String[] args) {
+		float a = 1234f;
+		float b = 532f;
+		float c = a / b;
+		System.out.printf("%-15.2f", c);
+	}
+
+}
