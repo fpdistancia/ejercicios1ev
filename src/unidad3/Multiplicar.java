c824426f0f8777ec1c7bcc4804fed7a02276177e
Julio Pedro López Bayón <jplbayon@gmail.com>
2020-11-04 18:39:25 +0100

    Ejercicio 5 de la unidad 3 parcialmente resuelto

---
+++ src/unidad3/Multiplicar.java
@@ -0,0 +1,38 @@
+package unidad3;
+
+import java.util.Scanner;
+
+public class Multiplicar {
+
+//	public static void main(String[] args) {
+//		
+//		Scanner in = new Scanner(System.in);
+//		System.out.println("Deseas repasar una tabla de multiplicar (s/n): ");
+//		String respuesta = in.nextLine();
+//		while (respuesta.equalsIgnoreCase("s")) {
+//			
+//			
+//			
+//			
+//			System.out.println("Deseas repasar otra tabla de multiplicar (s/n): ");
+//			respuesta = in.nextLine();
+//		}
+//
+//	}
+
+public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		String respuesta;
+		do {
+			
+			
+			
+			
+			System.out.println("Deseas repasar otra tabla de multiplicar (s/n): ");
+			respuesta = in.nextLine();
+		} while (respuesta.equalsIgnoreCase("s"));
+
+	}
+	
+}
