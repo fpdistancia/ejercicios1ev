61d91d3f71f7ff5a31e4efb5c8b4228b1e17b9c8
Julio Pedro López Bayón <jplbayon@gmail.com>
2020-11-11 16:53:27 +0100

    Ejercicio 5 unidad 3 resuelto

--- src/unidad3/Multiplicar.java
+++ src/unidad3/Multiplicar.java
@@ -3,36 +3,111 @@ package unidad3;
 import java.util.Scanner;
 
 public class Multiplicar {
-
-//	public static void main(String[] args) {
-//		
-//		Scanner in = new Scanner(System.in);
-//		System.out.println("Deseas repasar una tabla de multiplicar (s/n): ");
-//		String respuesta = in.nextLine();
-//		while (respuesta.equalsIgnoreCase("s")) {
-//			
-//			
-//			
-//			
-//			System.out.println("Deseas repasar otra tabla de multiplicar (s/n): ");
-//			respuesta = in.nextLine();
-//		}
-//
-//	}
-
-public static void main(String[] args) {
+	
+	static Scanner in = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		solucionA();
+	}
 		
-		Scanner in = new Scanner(System.in);
-		String respuesta;
+	static boolean deseaContinuar(String pregunta) {
+		System.out.println(pregunta);
+		String respuesta = in.nextLine();
+		return respuesta.equalsIgnoreCase("s");
+	}
+	
+	static void solucionA() {
+		int tabla;
+		int resultado;
+		int resultadoCorrecto;
+		int fallos;
+		while (deseaContinuar("Deseas repasar una tabla de multiplicar (s/n): ")) {
+			System.out.println("¿Qué tabla deseas repasar? (Teclea el número)");
+			tabla = Integer.parseInt(in.nextLine());
+			fallos = 0;
+			for (int i=1; i <= 9; i++) {
+				resultadoCorrecto = tabla * i;
+				System.out.println("¿" + tabla + " x " + i + "?");
+				resultado = Integer.parseInt(in.nextLine());
+				if (resultado == resultadoCorrecto) {
+					System.out.println("Correcto");
+				}
+				else {
+					fallos++;
+					System.out.println("Incorrecto. El resultado correcto es " + resultadoCorrecto);
+				}
+			}
+			
+			if (fallos < 2) {
+				System.out.println("Has aprobado");
+			}
+			else {
+				System.out.println("Has suspendido");
+			}			
+		}
+	}
+
+	
+	static void solucionB() {
+		int tabla;
+		int resultado;
+		int resultadoCorrecto;
+		int fallos;
 		do {
-			
-			
-			
-			
-			System.out.println("Deseas repasar otra tabla de multiplicar (s/n): ");
-			respuesta = in.nextLine();
-		} while (respuesta.equalsIgnoreCase("s"));
+			System.out.println("¿Qué tabla deseas repasar? (Teclea el número)");
+			tabla = Integer.parseInt(in.nextLine());
+			fallos = 0;
+			for (int i=1; i <= 9; i++) {
+				resultadoCorrecto = tabla * i;
+				System.out.println("¿" + tabla + " x " + i + "?");
+				resultado = Integer.parseInt(in.nextLine());
+				if (resultado == resultadoCorrecto) {
+					System.out.println("Correcto");
+				}
+				else {
+					fallos++;
+					System.out.println("Incorrecto. El resultado correcto es " + resultadoCorrecto);
+				}
+			}
+			if (fallos < 2) {
+				System.out.println("Has aprobado");
+			}
+			else {
+				System.out.println("Has suspendido");
+			}
+		} while (deseaContinuar("Deseas repasar otra tabla de multiplicar (s/n): "));
 
 	}
 	
+	static void solucionC() {
+		int tabla;
+		int resultado;
+		int resultadoCorrecto;
+		int fallos;
+		
+		for (;deseaContinuar("Deseas repasar una tabla de multiplicar (s/n): ");) {
+			System.out.println("¿Qué tabla deseas repasar? (Teclea el número)");
+			tabla = Integer.parseInt(in.nextLine());
+			fallos = 0;
+			for (int i=1; i <= 9; i++) {
+				resultadoCorrecto = tabla * i;
+				System.out.println("¿" + tabla + " x " + i + "?");
+				resultado = Integer.parseInt(in.nextLine());
+				if (resultado == resultadoCorrecto) {
+					System.out.println("Correcto");
+				}
+				else {
+					fallos++;
+					System.out.println("Incorrecto. El resultado correcto es " + resultadoCorrecto);
+				}
+			}
+			
+			if (fallos < 2) {
+				System.out.println("Has aprobado");
+			}
+			else {
+				System.out.println("Has suspendido");
+			}			
+		}
+	}
 }
